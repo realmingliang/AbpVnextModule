3f1e506a8bb923a55920317a32c622e1de03c1e4
xiaoliang1314 <171372668@qq.com>
2020-04-02 14:53:34 +0800

    修复问题

--- modules/identity/src/Tudou.Abp.Identity.Application.Contracts/Tudou/Abp/Identity/IIdentityClaimTypeAppService.cs
+++ modules/identity/src/Tudou.Abp.Identity.Application.Contracts/Tudou/Abp/Identity/IIdentityClaimTypeAppService.cs
@@ -7,7 +7,7 @@ using Volo.Abp.Application.Services;
 namespace Tudou.Abp.Identity
 {
     public interface IIdentityClaimTypeAppService : ICrudAppService<IdentityClaimTypeDto, Guid, GetIdentityClaimTypesInput, IdentityClaimTypeCreateDto, IdentityClaimTypeUpdateDto>
-    {
-        public Task<List<IdentityClaimTypeDto>> GetAll();
+    { 
+        Task<List<IdentityClaimTypeDto>> GetAll();
     }
 }
